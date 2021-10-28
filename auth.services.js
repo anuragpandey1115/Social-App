ae986d0cf3abd58b406d25b80e45b5bfc036a5e2
Your Name <you@example.com>
2021-10-28 09:57:47 +0530

    v4

--- auth.services.js
+++ auth.services.js
@@ -1,9 +1,7 @@
 const jwt = require('jsonwebtoken')
 
 exports.createToken =  function(payload,callback){
-    console.log("creating Token")
     jwt.sign(payload,"mysecret",{expiresIn:"1h"},function(error, token){
-        console.log("ahter jwt.sign")
         if(error){
             callback(error,null)
             console.log("errornnnnnn",error);
@@ -20,7 +18,7 @@ exports.isloggedin = function(req,res,next){
     jwt.verify(token,"mysecret", function(error,payload) {
         if(error){
             res.status(401).send({
-                error:"UnAuthorised User in logging"
+                error:"UnAuthorised User is logging"
             })
         }
         else{
